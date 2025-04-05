4fa1bc31e7e8e352f6c01215d4283567eb3f9335
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-04-05 11:55:08 +0530

    creatre the herop photo

--- src/Components/heroSection/HeroPic.jsx
+++ src/Components/heroSection/HeroPic.jsx
@@ -4,9 +4,9 @@ const HeroPic = () => {
   return (
     <div className="h-full flex items-center justify-center">
       <img
-        src="../public/HexaPic.png"
+        src="../public/pic.png"
         alt="profile-photo"
-        className="max-h-[450px] w-auto"
+        className="max-h-[450px] w-auto rounded-full"
       />
       <div className="absolute -z-10-flex justify-center animate-pulse">
         <PiHexagonThin className="md h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
