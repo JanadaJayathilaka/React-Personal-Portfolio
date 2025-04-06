16118ef28fb52281ea3668013cf00d1fae992bd5
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-04-06 11:39:12 +0530

    change form text to text area

--- src/Components/contactMeSection/ContactForm.jsx
+++ src/Components/contactMeSection/ContactForm.jsx
@@ -6,7 +6,13 @@ const ContactForm = () => {
       <form action="">
         <input type="text" placeholder="Your name" required />
         <input type="email" placeholder="Your Email" required />
-        <input type="text" placeholder="Message" rows="9" cols="50" required />
+        <textarea
+          type="text"
+          placeholder="Message"
+          rows="9"
+          cols="50"
+          required
+        />
         <button type="submit"></button>
       </form>
     </div>
