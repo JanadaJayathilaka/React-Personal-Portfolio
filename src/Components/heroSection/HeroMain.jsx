7b7a729b992d48ae54f5b67296f5bb7c4c77f5e3
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-04-07 15:18:22 +0530

    screen fix

--- src/Components/heroSection/HeroMain.jsx
+++ src/Components/heroSection/HeroMain.jsx
@@ -5,7 +5,7 @@ import HeroPic from "./heroPic";
 const HeroMain = () => {
   return (
     <div className="pt-40 pb-16">
-      <div className="flex screen-md:flex-row screen-sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative px-4">
+      <div className="flex md:flex-row sm:flex-col max-w-[1200px] mx-auto justify-between items-center relative px-4">
         <HeroText />
         <HeroPic />
       </div>
