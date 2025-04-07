0429d82fffc5aa111f0f7c92d3e1b6337612dbde
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-04-07 17:23:06 +0530

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 import NavBarMain from "./Components/navbar/NavBarMain";
-import HeroMain from "./Components/heroSection/HeroMain";
+
 import HeroGradient from "./Components/heroSection/HeroGradient";
 import SubHeroSection from "./Components/heroSection/SubHeroSection";
 import AboutMeMain from "./Components/aboutMeSection/AboutMeMain";
-
 import SkillsMain from "./Components/skillsSection/SkillsMain";
 import ExperienceMain from "./Components/experienceSection/ExperienceMain";
 import ProjectsMain from "./Components/projectsSection/ProjectsMain";
 import ContactMeMain from "./Components/contactMeSection/ContactMeMain";
 import FooterMain from "./Components/footer/FooterMain";
+import HeroMain from "./Components/heroSection/HeroMain";
 
 function App() {
   return (
